ab7c70ff80383c9edb620764a328922183bf0598
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2025-05-23 12:12:58 +0200

    test: update and add new tests
    Update existing test to expect for the new column to be returned.
    Add new tests to make sure the newly added column returns the expected data even if a data type is not declared or does not match any of SQLite recognised classes.

--- tests/testthat/test-column-info.R
+++ tests/testthat/test-column-info.R
@@ -17,6 +17,58 @@ test_that("can extract column info", {
     data.frame(
       name = names(df),
       type = vapply(df, typeof, character(1)),
+      .declared_type = c("INTEGER", "REAL", "TEXT", "BLOB"),
+      stringsAsFactors = FALSE,
+      row.names = NULL
+    )
+  )
+})
+
+
+test_that("results without any declared type return 'NA'", {
+  db <- memory_db()
+  on.exit(dbDisconnect(db))
+
+  res <- dbSendQuery(db, "SELECT 1 as one")
+  info <- dbColumnInfo(res)
+  dbClearResult(res)
+
+  expect_equal(
+    info,
+    data.frame(
+      name = "one",
+      type = "integer",
+      .declared_type = "NA",
+      stringsAsFactors = FALSE,
+      row.names = NULL
+    )
+  )
+})
+
+
+test_that("results with mixed declared/undeclared and existing/non-existent column type", {
+  db <- dbConnect(SQLite(), extended_types = TRUE)
+  on.exit(dbDisconnect(db))
+
+  dbExecute(db, "CREATE TABLE tmp (col_int integer,
+                                   col_real real,
+                                   col_text text,
+                                   col_date date,
+                                   col_none,
+                                   col_dummy foo,
+                                   col_test bar)")
+  dbExecute(db, "INSERT INTO tmp VALUES (1, 8.8, 'a', '2025-05-23', 'b', 3.5, TRUE)")
+
+  res <- dbSendQuery(db, "SELECT * FROM tmp")
+  info <- dbColumnInfo(res)
+  dbClearResult(res)
+
+  expect_equal(
+    info,
+    data.frame(
+      name = c("col_int", "col_real", "col_text", "col_date", "col_none", "col_dummy", "col_test"),
+      type = c("integer", "double", "character", "Date", "character", "double", "integer"),
+      .declared_type = c("INTEGER", "REAL", "TEXT", "date", "NA", "foo", "bar"),
       stringsAsFactors = FALSE,
       row.names = NULL
     )
